72f4d683f0a7bf25aed4de28fdd207a96e04ef20
Kate Deogenova <e.deogenova@yandex.ru>
2024-12-14 10:30:33 +0300

    init

---
+++ src/components/app/app.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react';
+import '../../styles/index.scss';
+import styles from './app.module.scss';
+
+const App: FC = () => {
+
+  return (
+    <div >
+      
+    </div>
+  )
+}
+
+export default App;
