5c6078b705dbab1588093a9ecfe92df8d97db123
Kate Deogenova <e.deogenova@yandex.ru>
2024-12-15 22:04:57 +0300

    refactor: component app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -2,11 +2,13 @@ import { FC } from 'react';
 import '../../styles/index.scss';
 import styles from './app.module.scss';
 
+import { ToDoPage } from '../page/todo-page/todo-page';
+
 const App: FC = () => {
 
   return (
-    <div >
-      
+    <div className={styles.app}>
+      <ToDoPage />
     </div>
   )
 }
