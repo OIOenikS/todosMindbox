4c13959740d383da8ed5d010f81158ab55fe4ed9
Kate Deogenova <e.deogenova@yandex.ru>
2024-12-16 03:51:31 +0300

    refactor: component ControlPanel

--- src/components/control-panel/control-panel.tsx
+++ src/components/control-panel/control-panel.tsx
@@ -1,36 +1,45 @@
-import { FC } from 'react'
-import { useState, ChangeEvent, FormEvent } from 'react'
-import { v4 as uuidv4 } from 'uuid'
-import { TTask } from '../../types/types'
-import { TFormProps } from './type'
+import { FC, useState } from 'react'
+import { TControlPanel } from './type'
 
 import styles from './control-panel.module.scss'
 
-export const ControlPanel: FC = () => {
+export const ControlPanel: FC<TControlPanel> = ({
+  itemsLeft, 
+  refClearBttn, 
+  setCurrentList,
+  currentList 
+}) => {
 
 	return (
     <div className={styles.container}>
       <p className={styles.status}>
-        {`2 items left`}
+        {`${itemsLeft} items left`}
       </p>
       <div className={styles.control}>
         <button 
-          className={styles.bttn} 
+          className={`${styles.bttn} ${
+            currentList === 'all' && styles['bttn-active']
+          }`}
           type="button"
+          onClick={() => setCurrentList('all')}
         >
           All
         </button>
         <button 
-          className={styles.bttn} 
+          className={`${styles.bttn} ${
+            currentList === 'active' && styles['bttn-active']
+          }`}
           type="button"
-          onClick={() => {}}
+          onClick={() => setCurrentList('active')}
         >
           Active  
         </button>
         <button 
-          className={styles.bttn} 
+          className={`${styles.bttn} ${
+            currentList === 'completed' && styles['bttn-active']
+          }`}
           type="button"
-          onClick={() => {}}
+          onClick={() => setCurrentList('completed')}
         >
           Completed
         </button>
@@ -38,7 +47,7 @@ export const ControlPanel: FC = () => {
       <button
         className={styles.bttn} 
         type="button"
-        onClick={() => {}}
+        ref={refClearBttn}
       >
         Clear completed
       </button>
--- src/components/control-panel/type.ts
+++ src/components/control-panel/type.ts
@@ -1,5 +1,8 @@
-import { TTask } from '../../types/types'
+import { TStatusToDoPage } from '../../types/types';
 
-export type TFormProps = {
-  setTasks: React.Dispatch<React.SetStateAction<TTask[]>>;
+export type TControlPanel = {
+  itemsLeft: number;
+  refClearBttn: React.MutableRefObject<HTMLButtonElement | null>
+  setCurrentList: React.Dispatch<React.SetStateAction<TStatusToDoPage>>;
+  currentList: TStatusToDoPage;
 }
